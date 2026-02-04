80f869af202c38004da767a90f059df0f719ae0e
Alysson Gabriel <alysson.gabriel61@gmail.com>
2026-02-03 23:23:20 -0400

    feat: install prisma client and mysql adapter

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma.service';
+import { UserDto } from './dto/users.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    constructor(private prisma: PrismaService){}
+
+    async create(data: UserDto) {
+        const usuario = await this.prisma.usuario.create({ data });
+    }
+}
