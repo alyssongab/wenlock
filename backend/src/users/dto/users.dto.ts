ea29f297ef47eac172ec533ae2b74e3bdea1cc6f
Alysson Gabriel <alysson.gabriel61@gmail.com>
2026-02-04 00:49:16 -0400

    refactor: create dto for user response

--- backend/src/users/dto/users.dto.ts
+++ backend/src/users/dto/users.dto.ts
@@ -1,7 +1,13 @@
-export type UserDto = {
+export type UserRequestDto = {
     id?: number,
     nome: string,
     matricula: number,
     email: string,
     senha: string
+}
+
+export type UserResponseDto = {
+    id: number,
+    nome: string,
+    email: string,
 }
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma.service';
-import { UserDto } from './dto/users.dto';
+import { UserRequestDto, UserResponseDto } from './dto/users.dto';
 
 @Injectable()
 export class UsersService {
 
     constructor(private prisma: PrismaService){}
 
-    async create(data: UserDto) {
+    async create(data: UserRequestDto): Promise<UserResponseDto> {
         const usuario = await this.prisma.usuario.create({ data });
+        return this.userToResponse(usuario);
+    }
+
+    private userToResponse(userRequest: UserRequestDto): UserResponseDto{
+        return {
+            id: userRequest.id,
+            nome: userRequest.nome,
+            email: userRequest.email
+        };
     }
 }
