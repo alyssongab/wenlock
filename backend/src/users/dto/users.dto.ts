ecbf65455c4ca2c39f7bf3f2096459b39592418a
Alysson Gabriel <alysson.gabriel61@gmail.com>
2026-02-04 03:40:42 -0400

    feat: add additional validation and pagination support to dtos

--- backend/src/users/dto/users.dto.ts
+++ backend/src/users/dto/users.dto.ts
@@ -1,29 +1,65 @@
-import { IsAlphanumeric, IsEmail, IsInt, IsString, MaxLength, MinLength, Min, Max } from "class-validator"
+import { IsAlphanumeric, IsEmail, IsInt, IsString, MaxLength, MinLength, Min, Max, IsOptional, Matches } from "class-validator"
+import { Type } from "class-transformer";
+// transformar string pra numero
 
 export class UserRequestDto  {
     
+    @IsOptional()
     @IsInt({message: "ID Inválido"})
-    id?: number
+    id?: number;
 
     @IsString({message: "O nome deve ser texto"})
     @MaxLength(30, {message: "Máximo 30 caracteres"})
-    nome: string
+    @Matches(/^[a-zA-Z\s]+$/, { message: "Nome deve conter apenas letras e espaços" })
+    nome: string;
 
     @IsInt({ message: "A matrícula deve ser um número inteiro" })
     @Min(1000, { message: "A matrícula deve ter no mínimo 4 dígitos" })
     @Max(9999999999, { message: "A matrícula deve ter no máximo 10 dígitos" })
-    matricula: number
+    matricula: number;
 
     @IsEmail({}, {message: "Email inválido"})
-    email: string
+    email: string;
 
     @IsAlphanumeric("pt-BR", {message: "Apenas caracteres alfanuméricos" })
     @MinLength(6, { message: "Mínimo 6 caracteres"})
-    senha: string
+    senha: string;
+}
+
+export class PaginationQueryDto {
+    @IsOptional()
+    @Type(() => Number)
+    @IsInt()
+    @Min(1)
+    page?: number;
+
+    @IsOptional()
+    @Type(() => Number)
+    @IsInt()
+    @Min(1)
+    limit?: number;
+
+    @IsOptional()
+    @IsString()
+    name?: string;
 }
 
 export class UserResponseDto  {
-    id: number
-    nome: string
-    email: string
+    id: number;
+    nome: string;
+    email: string;
+    createdAt: Date
+}
+
+export class PaginatedUserResponseDto {
+    data: UserResponseDto[];
+    meta: {
+        total: number;
+        page: number;
+        limit: number;
+        count: number;
+        search?: {
+            name?: string;
+        }
+    }
 }
