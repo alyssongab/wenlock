05a129a04b87e9ca427b879b8077db68435d6ff0
Alysson Gabriel <alysson.gabriel61@gmail.com>
2026-02-04 00:50:01 -0400

    feat: implement user creation endpoint in controller

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import type { UserRequestDto } from './dto/users.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    constructor(private readonly userService: UsersService){}
+
+    @Post()
+    async create(@Body() data: UserRequestDto){
+        return this.userService.create(data);
+    }
+}
