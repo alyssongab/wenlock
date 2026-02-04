3fd033186be3fb4fa588b6c87c573f457deea14e
Alysson Gabriel <alysson.gabriel61@gmail.com>
2026-02-03 22:06:12 -0400

    feat: add usuario model and first migration

---
+++ backend/prisma/migrations/20260204020502_add_mapping_to_usuario_model/migration.sql
@@ -0,0 +1,18 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Usuario` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE `Usuario`;
+
+-- CreateTable
+CREATE TABLE `usuarios` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `nome` VARCHAR(30) NOT NULL,
+    `email` VARCHAR(40) NOT NULL,
+    `senha` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
